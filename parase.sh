0f08fec9e686d825523190f172119b0334721021
hanyi <hanyi@2023>
2023-03-21 10:45:35 +0800

    shell script

---
+++ parase.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+path_input=./packet
+path_output=./result
+
+if [ ! -d $path_output ];then
+  mkdir $path_output
+fi
+
+files=$(ls $path_input)
+for filename in $files
+do
+ ./pcap-anlyze $path_input"/"${filename%.*}".pcap" > $path_output"/"${filename%.*}".txt"
+done
+echo $path_input": 解析结束"
